406204d8612ade4e64942cf6ea37d6dfbe71173b
mvpc-11232 <mvp2612@hotmail.com>
2022-12-15 08:45:29 +0530

    asdf

--- redis.tf
+++ redis.tf
@@ -5,7 +5,7 @@ resource "aws_elasticache_cluster" "redis" {
   node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
   parameter_group_name = aws_elasticache_parameter_group.pg.name
-  engine_version       = "6.2"
+  engine_version       = "6.x"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
   security_group_ids   = [aws_security_group.allows_redis.id]
@@ -14,7 +14,7 @@ resource "aws_elasticache_cluster" "redis" {
 # Creates Parameter Group from Elastic Cache Cluster
 resource "aws_elasticache_parameter_group" "pg" {
   name   = "roboshop-parameter-grp-${var.ENV}"
-  family = "redis6.2"
+  family = "redis6.x"
 }
 
 # Creates Subnet Group
