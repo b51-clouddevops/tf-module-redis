8a75cb5b7c03a981ef0bca0e02a3aedb709a601e
mvpc-11232 <mvp2612@hotmail.com>
2022-12-15 08:07:30 +0530

    Auto Push Commit

--- redis.tf
+++ redis.tf
@@ -1,8 +1,11 @@
+# Creates Elastic Cache Cluster
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "roboshop-redis-${var.ENV}"
   engine               = "redis"
-  node_type            = "cache.m4.large"
-  num_cache_nodes      = 2
-  parameter_group_name = "default.memcached1.4"
+  node_type            = "cache.t3.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = ????
   port                 = 11211
 }
+
+# Creates Parameter Group from E
