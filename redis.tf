535eed5867ba52bdb5556c64d9494c8abd27c8e1
mvpc-11232 <mvp2612@hotmail.com>
2022-12-20 09:06:21 +0530

    Auto Push Commit

--- redis.tf
+++ redis.tf
@@ -2,10 +2,10 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "roboshop-redis-${var.ENV}"
   engine               = "redis"
-  node_type            = var.DOCDB_PORT_INSTANCE_CLASS
+  node_type            = var.REDIS_PORT_INSTANCE_CLASS
   num_cache_nodes      = var.REDIS_NODE_COUNT
   parameter_group_name = aws_elasticache_parameter_group.pg.name
-  engine_version       = var.DOCDB_ENGINE_VERSION
+  engine_version       = var.REDIS_ENGINE_VERSION
   port                 = var.REDIS_PORT
   subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
   security_group_ids   = [aws_security_group.allows_redis.id]
